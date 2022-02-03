2ba51693a45dffdaf311683e47a4c3c9ea904d9d
Milena Djukanovic <milenadj1998@gmail.com>
2022-02-03 11:59:23 +0100

    Adds base entity. Closes #10

--- pet-clinic-data/src/main/java/com/example/smpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/smpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.example.smpetclinic.model;
 
-public class Person{
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/example/smpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/example/smpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.example.smpetclinic.model;
 
-public class PetType{
+public class PetType extends BaseEntity{
 
     private String name;
 
