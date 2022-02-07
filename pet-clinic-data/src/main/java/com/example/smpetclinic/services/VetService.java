36c4750fd4fa9b35736d292bdc4ac1c4b57dcda5
Milena Djukanovic <milenadj1998@gmail.com>
2022-02-07 18:41:38 +0100

    Adds bootstrap for data. Closes #14

--- pet-clinic-data/src/main/java/com/example/smpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/example/smpetclinic/services/VetService.java
@@ -2,7 +2,5 @@ package com.example.smpetclinic.services;
 
 import com.example.smpetclinic.model.Vet;
 
-import java.util.Set;
-
 public interface VetService extends CrudService<Vet, Long>{
 }
--- pet-clinic-data/src/main/java/com/example/smpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/smpetclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package com.example.smpetclinic.services.map;
 
 import com.example.smpetclinic.model.Owner;
-import com.example.smpetclinic.services.CrudService;
+import com.example.smpetclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long>{
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
     public Set<Owner> findAll(){
@@ -30,5 +30,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner findById(Long id){
         return super.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName){
+        return null;
     }
 }
