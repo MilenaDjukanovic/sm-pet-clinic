e566f1acbb51b2f4d672d52b6a7cb009b8d12d61
Milena Djukanovic <milenadj1998@gmail.com>
2022-02-25 13:46:15 +0100

    Adds contact info to Owner entity. Closes #27

--- pet-clinic-data/src/main/java/com/example/smpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/example/smpetclinic/model/Specialty.java
@@ -1,2 +1,14 @@
-package com.example.smpetclinic.model;public class Specialty {
+package com.example.smpetclinic.model;
+
+public class Specialty extends BaseEntity{
+
+    private String description;
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
 }
