d2b3b4f945c7df5752c465d662545a8ed87028c5
Milena Djukanovic <milenadj1998@gmail.com>
2022-03-30 13:32:46 +0200

    Updates base entity with JPA annotations. Closes #34

--- pet-clinic-data/src/main/java/com/example/smpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/example/smpetclinic/model/BaseEntity.java
@@ -1,9 +1,18 @@
 package com.example.smpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// Establishing this class as a base class(other classes will inherit it),
+// but we don't need this specific class mapped to the database
+@MappedSuperclass
 public class BaseEntity implements Serializable{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId(){
