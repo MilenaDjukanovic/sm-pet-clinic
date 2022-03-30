6c257e74d27677cf9cc1213604de04e2f9ae99e1
Milena Djukanovic <milenadj1998@gmail.com>
2022-03-30 15:01:47 +0200

    Adds spring data JPA for Vets. Closes #40

---
+++ pet-clinic-data/src/main/java/com/example/smpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,46 @@
+package com.example.smpetclinic.services.springdatajpa;
+
+import com.example.smpetclinic.model.Vet;
+import com.example.smpetclinic.repositories.VetRepository;
+import com.example.smpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Qualifier("vetsdjpaservice")
+public class VetSDJpaService implements VetService{
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository){
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll(){
+        return new HashSet<>(this.vetRepository.findAll());
+    }
+
+    @Override
+    public Vet findById(Long aLong){
+        return this.vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object){
+        return this.vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object){
+        this.vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong){
+        this.vetRepository.deleteById(aLong);
+    }
+}
