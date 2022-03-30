cab5f5553a8e1e9912a74738624b8857f0c5415d
Milena Djukanovic <milenadj1998@gmail.com>
2022-03-30 15:13:36 +0200

    Adds spring data JPA service for Specialities. Closes #43

---
+++ pet-clinic-data/src/main/java/com/example/smpetclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,46 @@
+package com.example.smpetclinic.services.springdatajpa;
+
+import com.example.smpetclinic.model.Speciality;
+import com.example.smpetclinic.repositories.SpecialityRepository;
+import com.example.smpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("specialitysdjpaservice")
+public class SpecialitySDJpaService implements SpecialtyService{
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository){
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll(){
+        return new HashSet<>(this.specialityRepository.findAll());
+    }
+
+    @Override
+    public Speciality findById(Long aLong){
+        return this.specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object){
+        return this.specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object){
+        this.specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong){
+        this.specialityRepository.deleteById(aLong);
+    }
+}
