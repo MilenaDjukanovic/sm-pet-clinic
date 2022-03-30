aab1eb59abfdb99ec70f51f80a4aef60c15222b9
Milena Djukanovic <milenadj1998@gmail.com>
2022-03-30 14:58:03 +0200

    Adds spring data JPA service for Owners. Closes #39

--- pet-clinic-data/src/main/java/com/example/smpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/example/smpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.example.smpetclinic.model.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OwnerRepository extends JpaRepository<Owner, Long>{
+
+    Owner findByLastName(String lastName);
 }
